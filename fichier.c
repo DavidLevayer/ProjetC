c0852a7ae7e2071cec424b2983b149aaf23437b9
Jerome <barbier_jerome@hotmail.fr>
2013-05-28 15:17:49 +0200

    modification de tout le fichier

--- fichier.c
+++ fichier.c
@@ -1,3 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+*
+*
+**/
+FILE* ouvrir (char* nomFichier)
+{
+	FILE* f = NULL;
+	f = fopen(nomFichier, "r");	
+		
+	if (f != NULL)
+  {
+  	return f;
+  }
+	else
+	{
+		printf("Erreur lors de l'ouverture du fichier %s\n",nomFichier);
+		return NULL;
+	}
+}
+
+
+
+/**
+*
+*
+**/
+int fermer (FILE* fichier)
+{
+	fclose(fichier);
+}
