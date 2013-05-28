a4ce74ec69c1dee7dc82a7d7b916607d9815ef90
Nexucis <nexucis@gmail.com>
2013-05-28 17:26:29 +0200

    mise à jour du dico avec nouvelle structure et peut etre signature modifier

--- dictionnaire.h
+++ dictionnaire.h
@@ -15,19 +15,43 @@
 
 typedef struct strCode {
 	char *valeur;
+        int taille;
 } Code; 
 
+/*
+ * @mot : chaine de caractere ascii + taille de la chaine
+ * @param : num est le numero du mot 
+ */
+
+typedef struct strCellule{
+    Code mot;
+    int num;
+    struct Cellule* nextp;
+}Cellule;
+/*
+ * @param : beginp : pointeur de debut de chaine
+ * @param : finalp : pointeur vers la dernière cellule
+ */
+typedef struct strDico{
+    Cellule* beginp;
+    Cellule* finalp;
+}Dico;
 /**
 *
 *
 **/
-int initialiser ();
+Dico initialiser ();
 
 /**
-*
+*ajoute l'élément s'il n'est pas présent ne fait rien sinon... retourne le numero du code que l'on souhaite ajouter
 *
 **/
-int ajouterElement (Code element, Code mono, Code *longueur);
+int ajouterElement (Code element, char* mono);
+
+/*
+ * 0 si pas trouver 1 sinon
+ */
+int rechercher(Code mot);
 
 /**
 *
@@ -39,4 +63,4 @@ char *codeVersChaine (Code code, int longueur);
 *
 *
 **/
-Code sequenceVersCode (char *sequence, int longueur);
+char* sequenceVersCode (Code sequence, int longueur);
